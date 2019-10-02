bdddfea005dc255df24fe69c2dd15eb4bf12a850
jcamilovelandiab <jcamilovelandiab@gmail.com>
2019-10-02 01:26:34 -0500

    nothing

--- src/component/Card/SingleCard.js
+++ src/component/Card/SingleCard.js
@@ -11,38 +11,37 @@ export default function RecipeReviewCard(props) {
   const classes = singleCardStyles();
 
   return (
-    <React.Fragment>
-    <Card className={classes.card}>
-      <CardHeader
-        avatar={
-          (props.status==="Ready")?<Avatar aria-label="recipe" className={classes.avatar}
-          style={{backgroundColor: "#f44336"}}>
-          R  </Avatar>:
-          (props.status==="Completed")?<Avatar aria-label="recipe" className={classes.avatar} style={{backgroundColor: "#36F485"}}>
-            C
-          </Avatar>:
-          <Avatar aria-label="recipe" className={classes.avatar} style={{backgroundColor: "#176BC2"}}>
-          IP
-          </Avatar>
-        }
-        title={props.description}
-      />
-      
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {props.status} - {props.dueDate}
-        </Typography>  
-      </CardContent>
 
-      <CardActions>
-        <Typography variant="body2" color="textSecondary" component="p">
-            {props.responsible.name}
-        </Typography>
-        <Typography variant="body2" color="textSecondary" component="p">
-            {props.responsible.email}
-        </Typography>
-      </CardActions>
-    </Card>
-    </React.Fragment>
+      <Card className={classes.card}>
+        <CardHeader
+          avatar={
+            (props.status==="Ready")?<Avatar aria-label="recipe" className={classes.avatar}
+            style={{backgroundColor: "#f44336"}}>
+            R  </Avatar>:
+            (props.status==="Completed")?<Avatar aria-label="recipe" className={classes.avatar} style={{backgroundColor: "#36F485"}}>
+              C
+            </Avatar>:
+            <Avatar aria-label="recipe" className={classes.avatar} style={{backgroundColor: "#176BC2"}}>
+            IP
+            </Avatar>
+          }
+          title={props.description}
+        />
+        
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.status} - {props.dueDate}
+          </Typography>  
+        </CardContent>
+
+        <CardActions>
+          <Typography variant="body2" color="textSecondary" component="p">
+              {props.responsible.name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+              {props.responsible.email}
+          </Typography>
+        </CardActions>
+      </Card>
   );
 }
